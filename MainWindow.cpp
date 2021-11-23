7563c93c0f393be171f34c67a96fe1feed13a1c2
ArcanusNEO <lucasding@foxmail.com>
2021-11-23 16:45:53 +0800

    .

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,15 +1,13 @@
 #include "MainWindow.h"
-#include "./ui_MainWindow.h"
+#include "ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget* parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+  delete ui;
 }
 
